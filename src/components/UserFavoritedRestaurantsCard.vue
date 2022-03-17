7cbc76afd9b8f699fd481f949d35436257f3015c
zarose0000ds <zarose0000ds@gmail.com>
2022-03-17 17:08:36 +0800

    feat: add User Profile page

---
+++ src/components/UserFavoritedRestaurantsCard.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="card">
+    <div class="card-header"><strong>0</strong> 收藏的餐廳</div>
+    <div class="card-body"></div>
+  </div>
+</template>
---
+++ src/components/UserProfileCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card mb-3">
+    <div class="row no-gutters">
+      <div class="col-md-4">
+        <img
+          src="https://via.placeholder.com/300"
+          width="300px"
+          height="300px"
+        />
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">user1</h5>
+          <p class="card-text">user1@example.com</p>
+          <ul class="list-unstyled list-inline">
+            <li><strong>31</strong> 已評論餐廳</li>
+            <li><strong>0</strong> 收藏的餐廳</li>
+            <li><strong>0</strong> followings (追蹤者)</li>
+            <li><strong>0</strong> followers (追隨者)</li>
+          </ul>
+          <p>
+            <a href="/users/2/edit"
+              ><button type="submit" class="btn btn-primary">edit</button></a
+            >
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
