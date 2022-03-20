4bd94d071aa906ecf13347889a74dddb40024955
zarose0000ds <zarose0000ds@gmail.com>
2022-03-21 00:29:51 +0800

    complete User Profile API concatenating

--- src/components/UserProfileCard.vue
+++ src/components/UserProfileCard.vue
@@ -18,10 +18,18 @@
             <li><strong>{{ profile.Followings.length }}</strong> followings (追蹤者)</li>
             <li><strong>{{ profile.Followers.length }}</strong> followers (追隨者)</li>
           </ul>
-          <p>
-            <router-link :to="{ name: 'user-edit', params: { id: profile.id } }" v-if="currentUserId === profile.id">
+          <p v-if="currentUser.id === profile.id">
+            <router-link :to="{ name: 'user-edit', params: { id: profile.id } }">
               <button type="submit" class="btn btn-primary">edit</button>
             </router-link>
+          </p>
+          <p class="mt-3" v-else>
+            <button type="button" class="btn btn-danger" v-if="isFollowed" @click="deleteFollowing(profile.id)">
+              取消追蹤
+            </button>
+            <button type="button" class="btn btn-primary" v-else @click="addFollowing(profile.id)">
+              追蹤
+            </button>
           </p>
         </div>
       </div>
@@ -30,29 +38,75 @@
 </template>
 
 <script>
-const dummyUser = {
-  currentUser: {
-    id: 1,
-    name: '管理者',
-    email: 'root@example.com',
-    image: 'https://i.pravatar.cc/300',
-    isAdmin: true
-  },
-  isAuthenticated: true
-}
+import usersAPI from '../apis/users'
+import { Toast } from '../utils/helpers'
+import { mapState } from 'vuex'
 
 export default {
   props: {
     initialProfile: {
       type: Object,
       required: true
+    },
+    initialIsFollowed: {
+      type: Boolean,
+      default: false
     }
   },
   data() {
     return {
-      currentUserId: dummyUser.currentUser.id,
-      profile: this.initialProfile
+      profile: this.initialProfile,
+      isFollowed: this.initialIsFollowed
     }
+  },
+  methods: {
+    async addFollowing(userId) {
+      try {
+        const { data } = await usersAPI.addFollowing({ userId })
+
+        if (data.status !== 'success') {
+          throw new Error(data.message)
+        }
+
+        const temp = {
+          ...this.currentUser,
+          Followship: {
+            followerId: this.currentUser.id,
+            followingId: this.profile.id
+          }
+        }
+
+        this.profile.Followers.push(temp)
+        this.isFollowed = true
+      } catch (err) {
+        Toast.fire({
+          icon: 'error',
+          title: '無法加入追蹤，請稍後再試'
+        })
+        console.log(err)
+      }
+    },
+    async deleteFollowing(userId) {
+      try {
+        const { data } = await usersAPI.deleteFollowing({ userId })
+
+        if (data.status !== 'success') {
+          throw new Error(data.message)
+        }
+
+        this.profile.Followers = this.profile.Followers.filter(follower => follower.id !== this.currentUser.id)
+        this.isFollowed = false
+      } catch (err) {
+        Toast.fire({
+          icon: 'error',
+          title: '無法取消追蹤，請稍後再試'
+        })
+        console.log(err)
+      }
+    }
+  },
+  computed: {
+    ...mapState(['currentUser'])
   }
 }
 </script>
